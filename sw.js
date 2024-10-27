09dd4d36c7cbe2903557d924221ea3c41bf3b8f1
aleembhd <106424234+aleembhd@users.noreply.github.com>
2024-10-28 00:14:23 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'studentconnect-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/webapp.html',
+  '/students.html',
+  '/dialpad.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/images/favicon.ico',
+  'https://i.postimg.cc/Xqp2r5bL/download-2.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css',
+  'https://www.gstatic.com/firebasejs/10.13.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.13.0/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
